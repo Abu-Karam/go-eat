aa83925fbd859bd0e3590c12e57f8cd37c96d7e9
anojleads <anojleads@gmail.com>
2020-03-18 16:26:11 +0100

    nav link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MenuComponent} from './menu/menu.component';
+import { GallaryComponent } from './gallary/gallary.component';
+
+
 
 
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'gallary', component: GallaryComponent}
 
 
-  { path: 'gallery', component: LandingPageComponent },
 
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
