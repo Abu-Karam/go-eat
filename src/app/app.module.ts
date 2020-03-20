64ca3efc8409f013525a36757eefbeb05e890ab9
Abu Karam <abu.karam@outlook.dk>
2020-03-21 00:01:35 +0100

    menu-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,9 @@ import { FindUsComponent } from './find-us/find-us.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IMeal } from './meal';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.filteredMeals = this.meals;
+    this._findText = 'fal';
+   }
 
   ngOnInit(): void {
   }
+  pageTitle: string = 'Menu List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  _findText: string;
+  get findText(): string {
+    return this._findText;
+  }
+  set findText(value: string) {
+    this._findText = value;
+    this.filteredMeals = this.findText ? this.searchMeals(this.findText) :this.meals;
+  }
+  filteredMeals: IMeal[];
+  meals: IMeal[] = [
+    {
+      "mealId": 1,
+      "mealName": "Durum shawarma",
+      "description": "Write some description",
+      "price": 39.99,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 4.5
+    },
+    {
+      "mealId": 2,
+      "mealName": "Falafel",
+      "description": "Write some description",
+      "price": 19.99,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 3.7
+    },
+    {
+      "mealId": 3,
+      "mealName": "Flæskesteg",
+      "description": "Write some description",
+      "price": 14.95,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 2.2
+    },
+    {
+      "mealId": 4,
+      "mealName": "Frikadeller",
+      "description": "Write some description",
+      "price": 49.95,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 4.1
+    },
+    {
+      "mealId": 5,
+      "mealName": "Salmon",
+      "description": "Write some description",
+      "price": 39.95,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 2.4
+    },
+    {
+      "mealId": 6,
+      "mealName": "Pizza",
+      "description": "Write some description",
+      "price": 29.95,
+      "imageUrl": "https://via.placeholder.com/300/09f/fff.png",
+      "stars": 4
+    }
+  ];
+
+
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+
+  }
+  searchMeals(byText: string): IMeal[] {
+    byText = byText.toLocaleLowerCase();
+    return this.meals.filter((meal: IMeal) =>
+    meal.mealName.toLocaleLowerCase().indexOf(byText) !== -1);
+  }
+
 
 }
